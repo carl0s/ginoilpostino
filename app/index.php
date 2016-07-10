bae60672a3055633b0aa5d1ef6448abe125a29f8
umbecanessa <umberto.canessa@gmail.com>
2016-07-10 04:59:40 +0200

    Agganciate index e cosa

--- app/index.php
+++ app/index.php
@@ -32,10 +32,152 @@ $upload_url = CloudStorageTools::createUploadUrl('/process.php', $options);
     <meta name="author" content="Team #keeppushing" />
     <link rel="shortcut icon" href="../favicon.ico"> 
     <link rel="stylesheet" type="text/css" href="dist/styles/app.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css">
     <script src="dist/scripts/modernizr.custom.js"></script>
     <script src="dist/scripts/webcamjs/webcam.js" type="text/javascript"></script>
     <script src="dist/scripts/trackingjs/build/tracking.js" type="text/javascript"></script>
+<style>
+.spinner {
+  width: 400px;
+  height: 150px;
+  font-size: 10px;
+  position: absolute;
+  right: 40px;
+  bottom: 0
+}
 
+.spinner > div {
+  background-color: #0047bb;
+  height: 100%;
+  width: 20px;
+  display: inline-block;
+  
+  -webkit-animation: sk-stretchdelay 1.8s infinite ease-in-out;
+  animation: sk-stretchdelay 1.8s infinite ease-in-out;
+}
+
+.spinner .rect2 {
+  -webkit-animation-delay: -1.7s;
+  animation-delay: -1.7s;
+}
+
+.spinner .rect3 {
+  -webkit-animation-delay: -1.6s;
+  animation-delay: -1.6s;
+}
+
+.spinner .rect4 {
+  -webkit-animation-delay: -1.5s;
+  animation-delay: -1.5s;
+}
+
+.spinner .rect5 {
+  -webkit-animation-delay: -1.4s;
+  animation-delay: -1.4s;
+}
+.spinner .rect6 {
+  -webkit-animation-delay: -1.3s;
+  animation-delay: -1.3s;
+}.spinner .rect7 {
+  -webkit-animation-delay: -1.2s;
+  animation-delay: -1.1s;
+}.spinner .rect8 {
+  -webkit-animation-delay: -1.0s;
+  animation-delay: -1.0s;
+}
+.spinner .rect9 {
+  -webkit-animation-delay: -0.9s;
+  animation-delay: -0.9s;
+}
+.spinner .rect10 {
+  -webkit-animation-delay: -0.8s;
+  animation-delay: -0.8s;
+}
+.spinner .rect11 {
+  -webkit-animation-delay: -0.7s;
+  animation-delay: -0.7s;
+}
+.spinner .rect12 {
+  -webkit-animation-delay: -0.6s;
+  animation-delay: -0.6s;
+}
+.spinner .rect13 {
+  -webkit-animation-delay: -0.5s;
+  animation-delay: -0.5s;
+}
+.spinner .rect14 {
+  -webkit-animation-delay: -0.4s;
+  animation-delay: -0.4s;
+}
+.spinner .rect15 {
+  -webkit-animation-delay: -0.3s;
+  animation-delay: -0.3s;
+}
+.spinner .rect16 {
+  -webkit-animation-delay: -0.2s;
+  animation-delay: -0.2s;
+}
+.spinner .rect17 {
+  -webkit-animation-delay: -0.1s;
+  animation-delay: -0.1s;
+}
+
+
+
+@-webkit-keyframes sk-stretchdelay {
+  0%, 40%, 100% { -webkit-transform: scaleY(0.4) }  
+  20% { -webkit-transform: scaleY(1.0) }
+}
+
+@keyframes sk-stretchdelay {
+  0%, 40%, 100% { 
+    transform: scaleY(0.4);
+    -webkit-transform: scaleY(0.4);
+  }  20% { 
+    transform: scaleY(1.0);
+    -webkit-transform: scaleY(1.0);
+  }
+}
+
+.hideright{
+left: 100%;
+
+}
+.hideleft{
+transform:translate(-100%,0);
+opacity: 0;
+
+}
+.shiftleft{
+transform:translate(0,0);
+
+}
+
+.coin{
+
+	width:42px;
+	height: 42px;
+	border-radius:50%;
+	border: 2px solid #fff;
+    position: absolute;
+    z-index: 10;
+    left: calc(50% - 21px);
+    top: calc(50% - 21px);
+
+
+}
+
+.square{
+
+height: 60px;
+width: 60px;
+background-color: #999;
+
+}
+
+
+}
+</style>
    </head>
 
 <body class="nl-blurred">
@@ -52,12 +194,32 @@ $upload_url = CloudStorageTools::createUploadUrl('/process.php', $options);
         </select>
       </div>
     </header>
-  <div class="camera">
+  <div class="camera" style="position:absolute; width:100%; transition: 1s; background-color:#fff; z-index:101">
   <div style="position:absolute; top:0; right:40px; width: 500px;text-align: right;">
-  	<p style="margin-top: -20px;font-size: 4em;">SCANNERIZZA IL TUO NUMERINO</p>
-  	<p style="margin-top: -20px;font-size: 2em; color:#ff867d; display:none" id="riprova">RIPROVA</p>
+  	<p style="margin-top: 0px;font-size: 4em;">SCANNERIZZA IL TUO NUMERINO</p>
+
+  	<p style="margin-top: -20px;font-size: 2em; color:#ff867d; display:none; font-weight: 700;" id="riprova">RIPROVA</p>
 
   </div>
+  <div class="spinner" style="display:none">
+  <div class="rect1"></div>
+  <div class="rect2"></div>
+  <div class="rect3"></div>
+  <div class="rect4"></div>
+  <div class="rect5"></div>
+  <div class="rect6"></div>
+  <div class="rect7"></div>
+  <div class="rect8"></div>
+  <div class="rect9"></div>
+  <div class="rect10"></div>
+  <div class="rect11"></div>
+  <div class="rect12"></div>
+  <div class="rect13"></div>
+  <div class="rect14"></div>
+  <div class="rect15"></div>
+  <div class="rect16"></div>
+  <div class="rect17"></div>  
+</div>
 <button class="next-slide" style="bottom:0" onClick="postphoto()">
 CHECK
 </button>
@@ -68,8 +230,30 @@ CHECK
     	<div class="left"></div>
     	<div class="right"></div>
     </div>-->
-    <div id="my_result" style="width:640px; height:240px;position:absolute;top: 0;left: 20px; z-index:5"></div>
+    <div id="my_result" style="width:640px; height:240px;position:absolute;margin: 20px 20px; top:0; z-index:5"></div>
   </div>
+
+<div id="shift" style="position:absolute; width:100%;height:520px;transition: 1s;background-color:#fff; z-index:99">
+<div id="ticket" style="width:300px; height:320px;box-shadow: 0px 0px 20px #333;left:150px; margin-top:100px;position:absolute; text-align:center">
+<p style="margin-bottom: 0;font-size:30px"><strong>Poste</strong>italiane</p>
+<p style="margin:0"><i>ANDRIA</i></p>
+<p id="service" style="margin-bottom: 0;"></p>
+<div style ="display:inline-block; position:relative;" class="square">
+<div class="coin" style="color:#fff">
+<p style="margin:0; font-size:30px; margin-top: -2px;" id="icn">€</p>
+</div>
+</div>
+<p id="numero" style="font-size: 65px;font-weight: 700;margin: 0;display:inline-block;    vertical-align: super;"></p>
+<p id="data" style="margin:0"></p>
+<p id="utenti"></p>
+</div>
+  <div style="position:absolute; top:0; right:40px; width: 500px;text-align: right; margin-top:20px">
+  	<p style="margin-top: 0px;font-size: 4em;">QUALCOSA MARKETTARO _ BLA BLA BLA ! DAJE</p>
+  </div>
+<button class="next-slide" style="bottom:0" onClick="javascript:window.location.href ='./cosa.html'">
+CONTINUA
+</button>
+</div>
     <script language="JavaScript">
 	
 	Webcam.set({
@@ -98,8 +282,12 @@ CHECK
 		
 	
 	var postphoto = function(){
-	
+	$('#riprova').hide();
+
 	take_snapshot();
+
+	$('.spinner').show()
+	$('.next-slide').attr('disabled');
 	var x = $('#photo').val();
 	var p = new Object;
 	p.img = x;
@@ -141,7 +329,7 @@ CHECK
 					}
 				var u = x - 1;
 				var b = data.responses[0].textAnnotations[u].description
-				console.log('Utenti attesa:' + b);
+				console.log('Utenti attesa: ' + b);
 				
 				 var today = new Date();
     var dd = today.getDate();
@@ -161,27 +349,45 @@ CHECK
 		var servizio = '';		
 		if(str.includes('FINA')){
 			
-			servizio = 'F'
+			servizio = 'FINANZIARIO'
 			
 			}else if(str.includes('SOL')){
 				
-				servizio = 'S'
+				servizio = '1 SOLA OPERAZIONE - 1 SOLO SERVIZIO'
 				
 				}else{
 					
-					servizio = 'P'
+					servizio = 'POSTALE E SERVIZI AL CITTADINO'
+					$('#icn').html('<i class="fa fa-envelope-o" aria-hidden="true"></i>');
+					$('.coin').css('border','none');
 					}		
 				console.log(servizio);
-			if(h !=='' && n !=='' && b !=='' && servizio !=='' && today !==''){console.log('DAICAZZO');}else{
+			if(h !=='' && n !=='' && b !=='' && servizio !=='' && today !==''){
+				$('#numero').html(n);
+
+
+
+				$('#service').html(servizio);
+				$('#utenti').html('Utenti attesa: ' + b);
+				$('#data').html(today + ' ' + h);
+
+				console.log('DAICAZZO');
+				$('.camera').addClass('hideleft');
+
+			}else{
 				$('img').removeAttr('src');
 				$('#riprova').show();
 				console.log('FANCULO');}
+				$('.spinner').hide()
+				$('.next-slide').removeAttr('disabled');
 
             },
 			error: function(e) {
 				console.log(e);
-$('img').removeAttr('src');
+				$('img').removeAttr('src');
 				$('#riprova').show();
+				$('.spinner').hide()
+				$('.next-slide').removeAttr('disabled');
 			}
        });
 		
